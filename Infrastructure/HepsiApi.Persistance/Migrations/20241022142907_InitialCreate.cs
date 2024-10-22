28f09e231c3bd83c13cc3b93026bf7fe55479981
Bera Yılmazyurt <sentinelprime33@gmail.com>
2024-10-22 18:01:58 +0300

    Created Dbcontext.

---
+++ Infrastructure/HepsiApi.Persistance/Migrations/20241022142907_InitialCreate.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HepsiApi.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Brands",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    isDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Brands", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ParentId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Priority = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    isDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BrandId = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Discount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    isDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Brands_BrandId",
+                        column: x => x.BrandId,
+                        principalTable: "Brands",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Details",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    isDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Details", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Details_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CategoryProduct",
+                columns: table => new
+                {
+                    CategoriesId = table.Column<int>(type: "int", nullable: false),
+                    ProductsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoryProduct", x => new { x.CategoriesId, x.ProductsId });
+                    table.ForeignKey(
+                        name: "FK_CategoryProduct_Categories_CategoriesId",
+                        column: x => x.CategoriesId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CategoryProduct_Products_ProductsId",
+                        column: x => x.ProductsId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Brands",
+                columns: new[] { "Id", "CreatedDate", "Name", "isDeleted" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 10, 22, 17, 29, 6, 659, DateTimeKind.Local).AddTicks(6621), "Sports & Electronics", false },
+                    { 2, new DateTime(2024, 10, 22, 17, 29, 6, 659, DateTimeKind.Local).AddTicks(6632), "Music", false },
+                    { 3, new DateTime(2024, 10, 22, 17, 29, 6, 659, DateTimeKind.Local).AddTicks(6644), "Movies & Jewelery", true }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "CreatedDate", "Name", "ParentId", "Priority", "isDeleted" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 10, 22, 17, 29, 6, 659, DateTimeKind.Local).AddTicks(9747), "Elektrik", 0, 1, false },
+                    { 2, new DateTime(2024, 10, 22, 17, 29, 6, 659, DateTimeKind.Local).AddTicks(9749), "Moda", 0, 2, false },
+                    { 3, new DateTime(2024, 10, 22, 17, 29, 6, 659, DateTimeKind.Local).AddTicks(9750), "Bilgisayar", 1, 1, false },
+                    { 4, new DateTime(2024, 10, 22, 17, 29, 6, 659, DateTimeKind.Local).AddTicks(9752), "Kadın", 2, 1, false }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Details",
+                columns: new[] { "Id", "CategoryId", "CreatedDate", "Description", "Title", "isDeleted" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(2024, 10, 22, 17, 29, 6, 662, DateTimeKind.Local).AddTicks(4087), "Doloremque kalemi suscipit enim koşuyorlar.", "Sokaklarda.", false },
+                    { 2, 3, new DateTime(2024, 10, 22, 17, 29, 6, 662, DateTimeKind.Local).AddTicks(4118), "Beatae dağılımı salladı sed de.", "Dolorem.", true },
+                    { 3, 4, new DateTime(2024, 10, 22, 17, 29, 6, 662, DateTimeKind.Local).AddTicks(4147), "Lakin mıknatıslı numquam quam koştum.", "Esse.", false }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "BrandId", "CreatedDate", "Description", "Discount", "Price", "Title", "isDeleted" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(2024, 10, 22, 17, 29, 6, 664, DateTimeKind.Local).AddTicks(2956), "Carbonite web goalkeeper gloves are ergonomically designed to give easy fit", 5.607562608262910m, 864.55m, "Generic Wooden Ball", false },
+                    { 2, 1, new DateTime(2024, 10, 22, 17, 29, 6, 664, DateTimeKind.Local).AddTicks(3011), "New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016", 2.287987281601210m, 419.21m, "Licensed Cotton Sausages", false }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryProduct_ProductsId",
+                table: "CategoryProduct",
+                column: "ProductsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Details_CategoryId",
+                table: "Details",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_BrandId",
+                table: "Products",
+                column: "BrandId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CategoryProduct");
+
+            migrationBuilder.DropTable(
+                name: "Details");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Brands");
+        }
+    }
+}
